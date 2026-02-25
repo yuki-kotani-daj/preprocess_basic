a9ffc3821aea3b45c136aec3adeacefec89b85ea
yuki-kotani-daj <260173797+yuki-kotani-daj@users.noreply.github.com>
2026-02-25 15:35:58 +0900

    Initial commit

---
+++ src/polars_extract_basic.py
@@ -0,0 +1,48 @@
+# 抽出
+import pandas as pd
+import polars as pl
+
+path = "/Users/yuki_kotani/data_science/preprocess_basic/data/reservation.parquet"
+
+## pandasを用いた抽出
+### []を用いる場合
+df = pd.read_parquet(path = path)
+df = df[["reservation_id","hotel_id","customer_id","checkin_date","checkout_date"]]
+print(df)
+
+### df.loc[]を用いる場合
+df = pd.read_parquet(path= path)
+df = df.loc[:,[
+    "reservation_id",
+    "hotel_id",
+    "customer_id",
+    "checkin_date",
+    "checkout_date"
+]]
+print(df)
+
+## polarsを利用した抽出
+df2 = pl.scan_parquet(path)
+
+query = (
+    df2
+    .select(pl.col(["reservation_id","hotel_id","customer_id","checkin_date","checkout_date"]))
+)
+
+print(query.collect())
+
+## 条件指定による列の抽出
+path2 = "/Users/yuki_kotani/data_science/preprocess_basic/data/hotel.parquet"
+
+### pandasを用いる場合
+pd_hotel = pd.read_parquet(path = path2)
+pd_hotel = pd_hotel.loc[:, lambda df3: df3.columns.str.startswith("tag_")]
+print(pd_hotel)
+
+### polarsを用いる場合
+df2 = pl.scan_parquet(path2)
+query = (
+    df2
+    .select(pl.col("^tag_.*$"))
+)
+print(query.collect())
