c49ac403edf7a979c0c9933e0c1db788c7700b80
yuki-kotani-daj <260173797+yuki-kotani-daj@users.noreply.github.com>
2026-02-26 15:39:48 +0900

    Add null_include_columns_extract

--- src/polars_extract_basic.py
+++ src/polars_extract_basic.py
@@ -40,9 +40,24 @@ pd_hotel = pd_hotel.loc[:, lambda df3: df3.columns.str.startswith("tag_")]
 print(pd_hotel)
 
 ### polarsを用いる場合
+#### polarsのpl.col関数で正規表現を用いた条件抽出を行う場合、必ず先頭を表す"^"で始まり、末尾を表す"$"を記載するルールを守る。
 df2 = pl.scan_parquet(path2)
 query = (
     df2
     .select(pl.col("^tag_.*$"))
 )
 print(query.collect())
+
+## 欠損のある列の抽出
+path3 = "/Users/yuki_kotani/data_science/preprocess_basic/data/customer.parquet"
+
+### pandasを用いる場合
+pd_customer = pd.read_parquet(path = path3)
+pd_customer = pd_customer.loc[:, lambda df3 : df3.isnull().any()]
+print(pd_customer)
+
+### polarsを用いる場合
+#### lazy_frameではfor文が使えない点に注意
+df2 = pl.read_parquet(path3)
+cols = [col.name for col in df2.select(pl.all().is_null()) if col.any()]
+print(df2.select(cols))
